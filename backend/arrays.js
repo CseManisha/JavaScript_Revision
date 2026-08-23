22e0bc7c264a6ded7caf065fc4644bdb8031a882
mani <manishamani1176@gmail.com>
2026-08-23 22:53:50 +0530

    missing number

--- backend/arrays.js
+++ backend/arrays.js
@@ -65,3 +65,11 @@ for(let num of nums){
     }
 }
 console.log(frequency);
+// find the missing number
+let number = [1, 2, 3, 5];
+
+for (let i = 1; i <= 5; i++) {
+    if (!number.includes(i)) {
+        console.log(i);
+    }
+}
